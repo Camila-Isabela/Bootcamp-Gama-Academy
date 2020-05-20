b585d1c4ba0733ef2d374babf6cdbdd839838694
Camila-00 <53484905+Camila-00@users.noreply.github.com>
2020-05-20 20:30:15 -0300

    :hammer: Modificando arquivos importantes

--- gamagit/src/App.js
+++ gamagit/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react'
 
-function App() {
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1>{props.title} {props.user}</h1>
+      <input name="usuario" id="usuario" className="usarioInput" placeholder="Usuário" />
+      <button type="button">Pesquisar</button>
+    </Fragment>
   );
 }
 
 export default App;
+
+//complicado fazer repo disso aqui!!
